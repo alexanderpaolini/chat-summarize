9539b387c3f02c7fee4a59b0ba6b62b150ea84ab
Alexander Paolini <30964205+alexanderpaolini@users.noreply.github.com>
2026-03-17 14:18:05 -0400

    chore(prompt): change prompt

--- src/lib/prompts.ts
+++ src/lib/prompts.ts
@@ -6,21 +6,21 @@ import { CommandOptions } from './commandParser';
 
 export const QUERY_SYSTEM_PROMPT =
   'Answer questions about the conversation.\n' +
-  '- Mention users as <@USER_ID> ex "<@277183033344524288>"\n' +
-  '- Mention channels as <#CHANNEL_ID> ex "<#1456394387268571169>"\n' +
+  '- Mention users as <@{USER_ID}> ex "<@277183033344524288>"\n' +
+  '- Mention channels as <#{CHANNEL_ID}> ex "<#1456394387268571169>"\n' +
   '- Link relevant attachments using their URLs\n' +
   '- Include source URLs inline at the end of the related sentence (same line)\n' +
   '- Content prefixed with "* " is metadata\n' +
-  '- Avoid filler phrases';
+  '- DO NOT use filler phrases';
 
 export const TLDR_SYSTEM_PROMPT =
   "Summarize the conversation -- essentially a TLDR for users who aren't caught up\n" +
-  '- Mention users as <@USER_ID> ex "<@277183033344524288>"\n' +
-  '- Mention channels as <#CHANNEL_ID> ex "<#1456394387268571169>"\n' +
+  '- Mention users as <@{USER_ID}> ex "<@277183033344524288>"\n' +
+  '- Mention channels as <#{CHANNEL_ID}> ex "<#1456394387268571169>"\n' +
   '- Link relevant attachments using their URLs\n' +
   '- Include source URLs inline at the end of the related sentence (same line)\n' +
   '- Content prefixed with "* " is metadata\n' +
-  '- Avoid filler phrases';
+  '- DO NOT use filler phrases';
 
 export const OVERALL_TLDR_PROMPT =
   '- Include an overall TLDR at the beginning without citations';
